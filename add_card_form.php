8f2240b2b89199a142015ed367a45478879557b1
desmond madden <desmondmad19@gmail.com>
2021-04-18 20:38:07 +0100

    added branch and contact stuff

--- add_card_form.php
+++ add_card_form.php
@@ -40,15 +40,69 @@ include('includes/header.php');
             <br>
 
             <label>Attribute:</label>
-            <input type="input" name="attribute">
+            <select name="attribute">
+                <option value="LIGHT">LIGHT</option>
+                <option value="DARK">DARK</option>
+                <option value="WIND">WIND</option>
+                <option value="WATER">WATER</option>
+                <option value="FIRE">FIRE</option>
+                <option value="EARTH">EARTH</option>
+                <option value="DIVINE">DIVINE</option>
+                <option value="SPELL">SPELL</option>
+                <option value="TRAP">TRAP</option>
+            </select>
             <br>
 
             <label>Type:</label>
-            <input type="input" name="type">
+            <select name="type">
+                <option value="Dragon">Dragon</option>
+                <option value="Spellcaster">Spellcaster</option>
+                <option value="Warrior">Warrior</option>
+                <option value="Machine">Machine</option>
+                <option value="Beast">Beast</option>
+                <option value="Beast-Warrior">Beast-Warrior</option>
+                <option value="Winged-Beast">Winged-Beast</option>
+                <option value="Wyrm">Wyrm</option>
+                <option value="Psychic">Psychic</option>
+                <option value="Thunder">Thunder</option>
+                <option value="Pyro">Pyro</option>
+                <option value="Aqua">Aqua</option>
+                <option value="Fairy">Fairy</option>
+                <option value="Cyberse">Cyberse</option>
+                <option value="Rock">Rock</option>
+                <option value="Reptile">Reptile</option>
+                <option value="Dinosaur">Dinosaur</option>
+                <option value="Zombie">Zombie</option>
+                <option value="Fish">Fish</option>
+                <option value="Sea-Serpent">Sea-Serpent</option>
+                <option value="Fiend">Fiend</option>
+                <option value="Divine-Beast">Divine-Beast</option>
+                <option value="Normal">Normal</option>
+                <option value="Equip">Equip</option>
+                <option value="Continuous">Continuous</option>
+                <option value="Quick-Play">Quick-Play</option>
+                <option value="Counter">Counter</option>
+                <option value="Ritual">Ritual</option>
+            </select>
             <br>
 
             <label>Card Type:</label>
-            <input type="input" name="cardType">
+            <select name="cardType">
+                <option value="Normal">Normal</option>
+                <option value="Normal/Pendulum">Normal/Pendulum</option>
+                <option value="Effect">Effect</option>
+                <option value="Effect/Pendulum">Effect/Pendulum</option>
+                <option value="Ritual">Ritual</option>
+                <option value="Fusion">Fusion</option>
+                <option value="Fusion/Pendulum">Fusion/Pendulum</option>
+                <option value="Synchro">Synchro</option>
+                <option value="Synchro/Pendulum">Synchro/Pendulum</option>
+                <option value="Xyz">Xyz</option>
+                <option value="Xyz/Pendulum">Xyz/Pendulum"</option>
+                <option value="Link">Link</option>
+                <option value="Spell">Spell</option>
+                <option value="Trap">Trap</option>
+            </select>
             <br>
 
             <label>Level:</label>
--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,9 @@
 <head>
 <title>Draw Phase Card Shop</title>
 <link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script>
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>Draw Phase Card Shop</h1></header>
+<header><h1>Draw Phase Card Shop and Collections</h1></header>
